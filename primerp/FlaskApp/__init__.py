95e5084cd6ef3a5efdd5fdc0974ad346ecc15c12
asalinas <asalinas@realnet.com.mx>
2019-06-07 18:08:17 -0500

    Navbar funcional

--- primerp/FlaskApp/__init__.py
+++ primerp/FlaskApp/__init__.py
@@ -13,7 +13,7 @@ dEmpresa = datosEmpresa()
 dTablas = Tablas()
 
 @app.route('/',methods=["POST","GET"])
-def dashboard():
+def analisis():
     estado = 1
     if request.method =="POST":
         initialDate = request.form['start']
@@ -25,14 +25,14 @@ def dashboard():
         if (graph==None):
             estado = 0
             flash("La busqueda no arrojo resultados. Intenta con diferente información")
-        return render_template("dashboard.html", Puntos=Puntos, grafica1=graph,initialDate=initialDate,estado=estado,empresa=empresa,dEmpresa=dEmpresa,dTablas=dTablas)
+        return render_template("/puntos/analisis.html", Puntos=Puntos, grafica1=graph,initialDate=initialDate,estado=estado,empresa=empresa,dEmpresa=dEmpresa,dTablas=dTablas)
     else:
         fechai = "2019-05-23"
         fechaf = "2019-05-29"
         empresa = ""
         graph = graph_1(fechai, fechaf,empresa)
 
-        return render_template("dashboard.html",Puntos=Puntos,grafica1=graph,estado=estado,dEmpresa=dEmpresa,dTablas=dTablas)
+        return render_template("/puntos/analisis.html",Puntos=Puntos,grafica1=graph,estado=estado,dEmpresa=dEmpresa,dTablas=dTablas)
 
 @app.route('/productividad',methods=["POST","GET"])
 def productividad():
@@ -56,10 +56,8 @@ def productividad():
 
         return render_template("/puntos/productividad.html",Puntos=Puntos,grafica1=graph,estado=estado,dEmpresa=dEmpresa,dTablas=dTablas)
 
-
-
-@app.route('/riesgos',methods=["POST","GET"])
-def riesgos():
+@app.route('/riesgoslegales',methods=["POST","GET"])
+def riesgoslegales():
     estado = 1
     if request.method =="POST":
         initialDate = request.form['start']
@@ -71,15 +69,172 @@ def riesgos():
         if (graph==None):
             estado = 0
             flash("La busqueda no arrojo resultados. Intenta con diferente información")
-        return render_template("/puntos/dashboard.html", Puntos=Puntos, grafica1=graph,initialDate=initialDate,estado=estado,empresa=empresa,dEmpresa=dEmpresa,dTablas=dTablas)
+        return render_template("/puntos/legales.html", Puntos=Puntos, grafica1=graph,initialDate=initialDate,estado=estado,empresa=empresa,dEmpresa=dEmpresa,dTablas=dTablas)
     else:
         fechai = "2019-05-23"
         fechaf = "2019-05-29"
         empresa = "FWFRNT"
         graph = graph_1(fechai, fechaf,empresa)
 
-        return render_template("/puntos/dashboard.html",Puntos=Puntos,grafica1=graph,estado=estado,dEmpresa=dEmpresa,dTablas=dTablas)
+        return render_template("/puntos/legales.html",Puntos=Puntos,grafica1=graph,estado=estado,dEmpresa=dEmpresa,dTablas=dTablas)
 
+@app.route('/fraudes',methods=["POST","GET"])
+def fraudes():
+    estado = 1
+    if request.method =="POST":
+        initialDate = request.form['start']
+        finalDate = request.form['end']
+        empresa = request.form['empresa']
+        print(initialDate,finalDate,empresa)
+        print(len(empresa))
+        graph = graph_1(initialDate, finalDate,empresa)
+        if (graph==None):
+            estado = 0
+            flash("La busqueda no arrojo resultados. Intenta con diferente información")
+        return render_template("/puntos/fraudes.html", Puntos=Puntos, grafica1=graph,initialDate=initialDate,estado=estado,empresa=empresa,dEmpresa=dEmpresa,dTablas=dTablas)
+    else:
+        fechai = "2019-05-23"
+        fechaf = "2019-05-29"
+        empresa = "FWFRNT"
+        graph = graph_1(fechai, fechaf,empresa)
+
+        return render_template("/puntos/fraudes.html",Puntos=Puntos,grafica1=graph,estado=estado,dEmpresa=dEmpresa,dTablas=dTablas)
+
+
+
+
+
+
+@app.route('/robo',methods=["POST","GET"])
+def robo():
+    estado = 1
+    if request.method =="POST":
+        initialDate = request.form['start']
+        finalDate = request.form['end']
+        empresa = request.form['empresa']
+        print(initialDate,finalDate,empresa)
+        print(len(empresa))
+        graph = graph_1(initialDate, finalDate,empresa)
+        if (graph==None):
+            estado = 0
+            flash("La busqueda no arrojo resultados. Intenta con diferente información")
+        return render_template("/puntos/robo.html", Puntos=Puntos, grafica1=graph,initialDate=initialDate,estado=estado,empresa=empresa,dEmpresa=dEmpresa,dTablas=dTablas)
+    else:
+        fechai = "2019-05-23"
+        fechaf = "2019-05-29"
+        empresa = "FWFRNT"
+        graph = graph_1(fechai, fechaf,empresa)
+
+        return render_template("/puntos/robo.html",Puntos=Puntos,grafica1=graph,estado=estado,dEmpresa=dEmpresa,dTablas=dTablas)
+
+@app.route('/lealtad',methods=["POST","GET"])
+def lealtad():
+    estado = 1
+    if request.method =="POST":
+        initialDate = request.form['start']
+        finalDate = request.form['end']
+        empresa = request.form['empresa']
+        print(initialDate,finalDate,empresa)
+        print(len(empresa))
+        graph = graph_1(initialDate, finalDate,empresa)
+        if (graph==None):
+            estado = 0
+            flash("La busqueda no arrojo resultados. Intenta con diferente información")
+        return render_template("/puntos/lealtad.html", Puntos=Puntos, grafica1=graph,initialDate=initialDate,estado=estado,empresa=empresa,dEmpresa=dEmpresa,dTablas=dTablas)
+    else:
+        fechai = "2019-05-23"
+        fechaf = "2019-05-29"
+        empresa = "FWFRNT"
+        graph = graph_1(fechai, fechaf,empresa)
+
+        return render_template("/puntos/lealtad.html",Puntos=Puntos,grafica1=graph,estado=estado,dEmpresa=dEmpresa,dTablas=dTablas)
+@app.route('/evasion',methods=["POST","GET"])
+def evasion():
+    estado = 1
+    if request.method =="POST":
+        initialDate = request.form['start']
+        finalDate = request.form['end']
+        empresa = request.form['empresa']
+        print(initialDate,finalDate,empresa)
+        print(len(empresa))
+        graph = graph_1(initialDate, finalDate,empresa)
+        if (graph==None):
+            estado = 0
+            flash("La busqueda no arrojo resultados. Intenta con diferente información")
+        return render_template("/puntos/evasion.html", Puntos=Puntos, grafica1=graph,initialDate=initialDate,estado=estado,empresa=empresa,dEmpresa=dEmpresa,dTablas=dTablas)
+    else:
+        fechai = "2019-05-23"
+        fechaf = "2019-05-29"
+        empresa = "FWFRNT"
+        graph = graph_1(fechai, fechaf,empresa)
+
+        return render_template("/puntos/evasion.html",Puntos=Puntos,grafica1=graph,estado=estado,dEmpresa=dEmpresa,dTablas=dTablas)
+
+@app.route('/bandwidth',methods=["POST","GET"])
+def bandwidth():
+    estado = 1
+    if request.method =="POST":
+        initialDate = request.form['start']
+        finalDate = request.form['end']
+        empresa = request.form['empresa']
+        print(initialDate,finalDate,empresa)
+        print(len(empresa))
+        graph = graph_1(initialDate, finalDate,empresa)
+        if (graph==None):
+            estado = 0
+            flash("La busqueda no arrojo resultados. Intenta con diferente información")
+        return render_template("/puntos/bandwidth.html", Puntos=Puntos, grafica1=graph,initialDate=initialDate,estado=estado,empresa=empresa,dEmpresa=dEmpresa,dTablas=dTablas)
+    else:
+        fechai = "2019-05-23"
+        fechaf = "2019-05-29"
+        empresa = "FWFRNT"
+        graph = graph_1(fechai, fechaf,empresa)
+
+        return render_template("/puntos/bandwidth.html",Puntos=Puntos,grafica1=graph,estado=estado,dEmpresa=dEmpresa,dTablas=dTablas)
+
+@app.route('/politicas',methods=["POST","GET"])
+def politicas():
+    estado = 1
+    if request.method =="POST":
+        initialDate = request.form['start']
+        finalDate = request.form['end']
+        empresa = request.form['empresa']
+        print(initialDate,finalDate,empresa)
+        print(len(empresa))
+        graph = graph_1(initialDate, finalDate,empresa)
+        if (graph==None):
+            estado = 0
+            flash("La busqueda no arrojo resultados. Intenta con diferente información")
+        return render_template("/puntos/politicas.html", Puntos=Puntos, grafica1=graph,initialDate=initialDate,estado=estado,empresa=empresa,dEmpresa=dEmpresa,dTablas=dTablas)
+    else:
+        fechai = "2019-05-23"
+        fechaf = "2019-05-29"
+        empresa = "FWFRNT"
+        graph = graph_1(fechai, fechaf,empresa)
+
+        return render_template("/puntos/politicas.html",Puntos=Puntos,grafica1=graph,estado=estado,dEmpresa=dEmpresa,dTablas=dTablas)
+
+@app.route('/reportes',methods=["POST","GET"])
+def reportes():
+    estado = 1
+    if request.method =="POST":
+        initialDate = request.form['start']
+        finalDate = request.form['end']
+        empresa = request.form['empresa']
+        print(initialDate,finalDate,empresa)
+        print(len(empresa))
+        graph = graph_1(initialDate, finalDate,empresa)
+        if (graph==None):
+            estado = 0
+            flash("La busqueda no arrojo resultados. Intenta con diferente información")
+        return render_template("/puntos/reportes.html", Puntos=Puntos, grafica1=graph,initialDate=initialDate,estado=estado,empresa=empresa,dEmpresa=dEmpresa,dTablas=dTablas)
+    else:
+        fechai = "2019-05-23"
+        fechaf = "2019-05-29"
+        empresa = "FWFRNT"
+        graph = graph_1(fechai, fechaf,empresa)
+
+        return render_template("/puntos/reportes.html",Puntos=Puntos,grafica1=graph,estado=estado,dEmpresa=dEmpresa,dTablas=dTablas)
 
 
 if __name__ == "__main__":
